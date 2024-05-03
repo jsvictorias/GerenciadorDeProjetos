78540df15e28141d8b6d000ab6c767b577555be0
jsvictorias <juliasantos7652@gmail.com>
2024-05-03 15:10:03 -0300

    `new:` adding the content to the box

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,11 +1,18 @@
 import React from "react";
 import '../components/components.css';
+import TPlist from "./lists";
 
 const Block = () => {
     return (
         <div className="bloch">
-            <div className="main">
-                
+            <div className="title-bloch">Meus projetos:</div>
+            <div className="main-cointainer">
+                <div className="main">
+                    <h4>PARA COMEÇAR</h4>
+                    <h4>FAZENDO</h4>
+                    <h4>FINALIZADO</h4>
+                </div>
+                <TPlist />
             </div>
         </div>
     )
